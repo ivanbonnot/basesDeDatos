e058bd37aca8ff6fe3816ea9528233061d2091ff
Ivan Bonnot <ivanbonnot@gmail.com>
2023-01-17 13:47:55 -0300

    casi listo

--- src/contenedor.js
+++ src/contenedor.js
@@ -1,87 +1,113 @@
+const knex = require('knex')
 
-const fs = require('fs')
-const { v4: uuidv4 } = require('uuid');
 
-class Contenedor {
+class Container {
 
-    constructor( file ) {
-        this.file = file
+  constructor( config, dbTable ) {
+      this.config = config
+      this.dbTable = dbTable
+}
+  
+  async getAll() {
+    try{
+      const itemsInDb = await knex(this.config).from(this.dbTable).select('*')
+      knex(this.config).destroy()
+      return itemsInDb
+    } catch(err) {
+      console.log(`Error: ${err}`)
     }
-
-
-    async save(product) {
-        const productos = await this.getAll()
-        try {
-            let idGen = uuidv4()
-
-            const prodNuevo = { id: idGen, ...product }
-            productos.push(prodNuevo)
-            await this.saveFile(productos)
-            return idGen
-
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
+  }
+ 
+  
+  async add( item ) {
+    try{
+      await knex(this.config)(this.dbTable).insert( item )
+      knex(this.config).destroy()
+      return
+    } catch(err) {
+      console.log(`Error: ${err}`)
     }
+  }
 
 
-    async saveFile( productos) {
-        try {
-            await fs.promises.writeFile(
-                this.file, JSON.stringify(productos, null, 2)
-            )
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
+  async modifyById( id, item ) {  
+    try {
+      const correctId = await knex(this.config)(this.dbTable).where('id', id).count()
+      if (correctId[0]['count(*)'] > 0){
+        await knex(this.config)(this.dbTable).where('id', id).update( item )
+        knex(this.config).destroy()
+        return true
+      } else {
+        knex(this.config).destroy()
+        return false
+      }
+    } catch(err) {
+      console.log(`Error: ${err}`)
+      return false
     }
+  }
 
 
-    async getById(id) {
-        const productos = await this.getAll()
-        try {
-            const prod = productos.find(item => item.id === id)
-            return prod ? prod : null
+  async getById( id ) {
+    try {
+      const product = await knex(this.config).select('title', 'price', 'thumbnail')
+      .from(this.dbTable).where('id', id).first()
+      console.log(product)
+      return product ? product : null
 
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
+    } catch(err) {
+      console.log(`Error: ${err}`)
     }
-    
+  }
 
-    async getAll() {
-        try {
-            const productos = await fs.promises.readFile(this.file, 'utf-8')
-            return JSON.parse(productos)
 
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
+  async deleteById( id ) {  
+    try {
+      const correctId = await knex(this.config)(this.dbTable).where('id', id).count()
+      if (correctId[0]['count(*)'] > 0){
+        await knex(this.config)(this.dbTable).where('id', id).del()
+        knex(this.config).destroy()
+        return true
+      } else {
+        knex(this.config).destroy()
+        return false
+      }
+    } catch(err) {
+      console.log(`Error: ${err}`)
+      return false
     }
+  }
 
 
-    async deleteById(id) {
-        let productos = await this.getAll()
-
-        try {
-            productos = productos.filter(item => item.id != id)
-            await this.saveFile(this.file, productos)
-
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
-    }
-
-
-    async deleteAll() {
-        await this.saveFile(this.file, [])
-    }
+  async deleteAll() {
+    await knex(this.config)(this.dbTable).del()
+  }
 
 }
 
 
+//------- Declaracion de instancias
 
-const products = new Contenedor('./data/productos.txt')
-const chat = new Contenedor('./data/chat.txt')
+const chat = new Container( 
+  {
+      client: 'sqlite3',
+      connection: { filename: './db/sqlite3db/ecommerce.sqlite' },
+      useNullAsDefault: true
+  },
+  'chat'
+)
+
+const products = new Container(
+  {
+      client: 'mysql',
+      connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        database: 'ecommerce'}
+  },
+  'products'
+)
 
 
-module.exports = { products, chat }
+
+module.exports = { chat, products }
